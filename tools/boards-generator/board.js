18f7ef8cdb704f1b401e011218e79c00e245a83b
Maxwin-z <zwd2005@gmail.com>
2017-11-04 10:20:43 +0800

    console log result

--- tools/boards-generator/board.js
+++ tools/boards-generator/board.js
@@ -1,50 +1,50 @@
-var href = window.location.href;
+var href = window.location.href
 
 function ls(key, value) {
-	if (value === undefined) {
-		var v = localStorage[key];
-		try {
-			v = JSON.parse(v);
-		} catch(ignore) {}
-		return v;
-	} else {
-		localStorage[key] = JSON.stringify(value);
-	}
+  if (value === undefined) {
+    var v = localStorage[key]
+    try {
+      v = JSON.parse(v)
+    } catch (ignore) {}
+    return v
+  } else {
+    localStorage[key] = JSON.stringify(value)
+  }
 }
 
 if (href.indexOf('http://m.newsmth.net/section') == 0) {
-	// init
-	if (href == 'http://m.newsmth.net/section') {
-		ls('boards', []);
-		ls('sections', []);
-	}
+  // init
+  if (href == 'http://m.newsmth.net/section') {
+    ls('boards', [])
+    ls('sections', [])
+  }
 
-	var sections = ls('sections');
-	var boards = ls('boards');
+  var sections = ls('sections')
+  var boards = ls('boards')
 
-	var as = document.querySelectorAll('.slist.sec a');
-	for (var i = 0; i != as.length; ++i) {
-		var a = as[i];
-		if (a.pathname.indexOf('/section/') == 0) {
-			sections.push(a.href);
-		} else if (a.pathname.indexOf('/board/') == 0) { // board
-			boards.push({
-				name: a.pathname.match(/\/([^\/]+$)/)[1],
-				cnName: a.innerHTML
-			});
-		}
-	}
+  var as = document.querySelectorAll('.slist.sec a')
+  for (var i = 0; i != as.length; ++i) {
+    var a = as[i]
+    if (a.pathname.indexOf('/section/') == 0) {
+      sections.push(a.href)
+    } else if (a.pathname.indexOf('/board/') == 0) {
+      // board
+      boards.push({
+        name: a.pathname.match(/\/([^\/]+$)/)[1],
+        cnName: a.innerHTML
+      })
+    }
+  }
 
-	var next = sections.shift();
+  var next = sections.shift()
 
-	// save sections & boards
-	ls('sections', sections);
-	ls('boards', boards);
+  // save sections & boards
+  ls('sections', sections)
+  ls('boards', boards)
 
-	if (next) {
-		window.location = next;
-	} else {
-		console.log(boards);
-	}
+  if (next) {
+    window.location = next
+  } else {
+    console.log(JSON.stringify(boards))
+  }
 }
-
